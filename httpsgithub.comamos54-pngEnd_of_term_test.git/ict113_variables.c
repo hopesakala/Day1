83f50f0f209567e388b4529ada1da7f5421f43a6
Amos54-png <amoslumba1@gmail.com>
2025-12-24 18:09:13 +0200

    Upadate ict113_variables.c with code

---
+++ httpsgithub.comamos54-pngEnd_of_term_test.git/ict113_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+   char name[50] =" Amos";
+   int age = 0;
+   float gpa= 0.0f;
+
+    age = 21;
+    gpa = 3.8f;
+
+  printf("Name: %s\n", name);
+  printf("Age: %d\n", age);
+  printf("Gpa: %.2f\n", gpa);
+
+return 0;
+}
