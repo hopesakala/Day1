081951ae7f2c28e0835f26c170ca5d530ca986b1
Amos54-png <amoslumba1@gmail.com>
2026-01-11 16:15:08 +0200

    Day1

---
+++ Day1/project3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+
+    scanf("%d", &num2);
+
+    printf("Sum = %d\n", num1 + num2);
+
+    printf("Difference = %d\n", num1 - num2);
+
+    printf("Product = %d\n", num1 * num2);
+
+    if(num2 != 0) {
+        printf("Quotient = %d\n", num1 / num2);
+
+    } else {
+        printf("Error: Division by zero!\n");
+
+    }
+
+    return 0;
+}
